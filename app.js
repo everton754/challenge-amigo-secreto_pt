262c7cb7c90e06768fd2473769936169b01d1f7c
Everton Santos <bio.e.s.o@gmail.com>
2025-03-06 11:51:41 -0300

    Inicializando projeto: Adicionando estrutura básica e README

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Variáveis para armazenar a lista de nomes e o nome sorteado
+let nomes = [];
+let nomeSorteado = "";
+
+// Função para adicionar nomes à lista
+function adicionarAmigo() {
+  const nome = document.getElementById("amigo").value.trim();
+  const regex = /^[A-ZáéíóúÁÉÍÓÚâêîôûÂÊÎÔÛãõÃÕçÇ][a-záéíóúâêîôûãõçÇ ]*$/;
+
+  if (nome !== "" && regex.test(nome)) {
+    nomes.push(nome);
+    document.getElementById("listaAmigos").innerHTML += `<li>${nome}</li>`;
+    document.getElementById("amigo").value = "";
+  } else {
+    alert(
+      "Por favor, insira um nome válido! (Apenas letras e acentos são permitidos, a primeira letra precisa ser maiúscula.)"
+    );
+  }
+}
+
+// Função para sortear um nome aleatório
+function sortearAmigo() {
+  if (nomes.length > 0) {
+    const indiceSorteado = Math.floor(Math.random() * nomes.length);
+    nomeSorteado = nomes[indiceSorteado];
+    document.getElementById(
+      "resultado"
+    ).innerHTML = `<li>O amigo secreto é: ${nomeSorteado}</li>`;
+  } else {
+    alert("Não há nomes na lista para sortear!");
+  }
+}
